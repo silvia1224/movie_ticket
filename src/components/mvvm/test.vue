9960572d7b7ac912c42c28919592cf310e8fa839
silvia1224 <1720254870@qq,com>
2019-10-14 09:58:02 +0800

    updated

---
+++ src/components/mvvm/test.vue
@@ -0,0 +1,36 @@
+<template>
+<div class='test'>
+    <input :value='val' @input='val=$event.target.value'>
+    <span>{{val}}</span>
+</div>
+    
+</template>
+<script>
+export default {
+  data(){
+      return{
+          val:'sadas'
+      }
+  },
+  methods:{
+    change(e){
+      console.log(e)
+    }
+  }
+    
+}
+</script>
+<style scoped lang="stylus">
+  .test
+    input 
+      width:30rem
+      height 3rem
+      background:#d1d1d1
+      font-size:2rem
+    span 
+      color:#afa
+      font-size:3rem
+      display:block
+</style>
+
+
